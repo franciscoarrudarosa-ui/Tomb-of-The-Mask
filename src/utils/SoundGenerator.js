43d395cc18960a984bf93c2f3593edc14788dc95
franciscoarrudarosa-ui <franciscoarrudarosa@gmail.com>
2026-05-13 19:38:00 -0300

    Update SoundGenerator.js

--- src/utils/SoundGenerator.js
+++ src/utils/SoundGenerator.js
@@ -71,6 +71,42 @@ export class SoundGenerator {
 
   playSelect() { this._playTone(660, 0.08, 'square', 0.08); }
   playTideWarning() { this._playTone(150, 0.3, 'sine', 0.06, false); }
+  
+  playTeleport() {
+    this._playTone(800, 0.1, 'sine', 0.1);
+    setTimeout(() => this._playTone(1200, 0.15, 'sine', 0.1), 50);
+  }
+
+  playCrumble() {
+    this._playNoise(0.2, 0.15);
+    this._playTone(100, 0.2, 'sawtooth', 0.1);
+  }
+
+  playDart() {
+    this._playNoise(0.05, 0.1);
+    this._playTone(1500, 0.05, 'triangle', 0.1);
+  }
+
+  playFreeze() {
+    this._playTone(1200, 0.3, 'sine', 0.1);
+    setTimeout(() => this._playNoise(0.3, 0.05), 50);
+  }
+
+  playShield() {
+    this._playTone(400, 0.1, 'square', 0.1);
+    setTimeout(() => this._playTone(600, 0.2, 'square', 0.1), 100);
+  }
+
+  playShieldBreak() {
+    this._playNoise(0.3, 0.2);
+    this._playTone(200, 0.2, 'sawtooth', 0.15);
+  }
+
+  playMagnet() {
+    this._playTone(500, 0.1, 'triangle', 0.1);
+    setTimeout(() => this._playTone(700, 0.1, 'triangle', 0.1), 100);
+    setTimeout(() => this._playTone(900, 0.2, 'triangle', 0.1), 200);
+  }
 }
 
 export const soundGen = new SoundGenerator();
